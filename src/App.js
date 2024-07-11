c6d1d1e6e1ef28686e5e0a08aa0c8bdaa0d0f158
Mahmut Can Çokçapar <mahmutcancokcapar@gmail.com>
2024-07-11 23:40:47 +0300

    Added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [counter, setCounter] = useState(0);
+	const [inputValue, setInputValue] = useState(0);
+
+	const handleInputChange = (event) => {
+		setInputValue(Number(event.target.value));
+	};
+
+	const arttir = () => {
+		setCounter(counter + inputValue);
+	};
+
+	const azalt = () => {
+		setCounter(counter - inputValue);
+	};
+
+	const sifirla = () => {
+		setCounter(0);
+	};
+
+	return (
+		<div className="App">
+			<h1>Counter App</h1>
+			<div id="sonuc">{counter}</div>
+			<div className="first-container">
+				<button onClick={arttir}>+</button>
+				<button onClick={azalt}>-</button>
+				<button onClick={sifirla}>0</button>
+			</div>
+			<input
+				type="number"
+				value={inputValue}
+				onChange={handleInputChange}
+				style={{ fontSize: 20, width: 50, textAlign: "center", marginTop: 20 }}
+			/>
+		</div>
+	);
+};
 
 export default App;
